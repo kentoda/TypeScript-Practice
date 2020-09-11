f39139084ae799d221b1286e955626bf042e74c9
kentoda <kent19950811@gmail.com>
2020-09-11 16:04:23 +0900

    First Typescript Lesson

---
+++ HelloWorld.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string): void{
+  console.log(`goodbye $(name)!`);
+}
+
+
+sayHello('Dave');
---
+++ Sample01.ts
@@ -0,0 +1,5 @@
+const x = 20;
+const y  = 5;
+
+let answer = x + y;
+console.log(answer);
